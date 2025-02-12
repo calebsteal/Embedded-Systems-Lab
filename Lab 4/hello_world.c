d94b5202f1c50236b64579ddf4ce3d332b04d75f
Spencer Dalgety <129558639+Spenceaway@users.noreply.github.com>
2025-02-11 18:16:51 -0600

    Add files via upload
    Added relevant lab 4 files:
    - PIT.c
    - fsl_pit.c
    - fsl_pit.h
    - Updated Hello_World.c with PIT support

---
+++ Lab 4/hello_world.c
@@ -0,0 +1,98 @@
+/*
+ * Copyright (c) 2013 - 2015, Freescale Semiconductor, Inc.
+ * Copyright 2016-2017 NXP
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include "fsl_device_registers.h"
+#include "fsl_debug_console.h"
+#include "pin_mux.h"
+#include "clock_config.h"
+#include "board.h"
+
+#include "fsl_port.h"
+#include "fsl_common.h"
+/*******************************************************************************
+ * Definitions
+ ******************************************************************************/
+
+
+/*******************************************************************************
+ * Prototypes
+ ******************************************************************************/
+
+/*******************************************************************************
+ * Code
+ ******************************************************************************/
+/*!
+ * @brief Main function
+ */
+int main(void)
+{
+	char ch;
+	char instring[20];
+int i = 0;
+
+	/* Init board hardware. */
+	BOARD_InitBootPins();
+	BOARD_InitBootClocks();
+	BOARD_InitDebugConsole();
+
+	PRINTF("hello world.\r\n");
+
+	while (1)
+	{
+		i=0;
+	    while (1==1)
+	    {
+	    	ch = GETCHAR();
+	    	instring[i++] = ch;
+	    	while ((ch != '\n') && (i<20))
+	    	{
+	    		ch = GETCHAR();
+	    		instring[i++] = ch;
+	    		GPIO_PortToggle(GPIOA, 1u << 1);
+	    	}
+
+	    	instring[i] = 0;
+	    	i = 0;
+
+	    	while (instring[i] != 0){
+	    		PUTCHAR(instring[i++]);
+	    	}
+
+	    	switch (instring[0]){
+	    	case 'a':
+	    	PRINTF("\rCommand 'a' detected\n\r");
+	    	GPIO_PortToggle(GPIOD, 1u << 5);
+	    	break;
+
+	    	case 'b':
+	    	PRINTF("\rCommand 'b' detected\n\r");
+	    	EnableIRQ(PORTC_IRQn);
+	    	while(1==1){}								// Fudge function - GETCHAR has a bad habit of locking up the Kinetis
+	    	break;
+
+	    	case 'p':
+	    	PRINTF("\rCommand 'p' detected\n\r");
+			EnableIRQ(PORTC_IRQn);
+			PIT_INIT();
+			while(1==1){}
+			break;
+
+	    	default:
+	    		PRINTF("\rUnrecognized command\n\r");
+	    		break;
+	    	} // end case
+
+	    	i = 0;
+	    }
+	}
+}
+
+void PORTC_IRQHandler(void){
+	PRINTF("Why is Dr. Russ always out of town :(\n");
+	PORT_ClearPinsInterruptFlags(PORTC, (1U << 1));
+}
